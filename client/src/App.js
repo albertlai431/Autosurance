4b8d2ac417c5456c9ded9587e6c7bd9e352eb290
William Law <horizon.william14@gmail.com>
2019-08-24 10:22:33 -0400

    Initial commit for react

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Styling
+import "./App.css";
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Autosurance</h1>
+    </div>
+  );
+}
+
+export default App;
